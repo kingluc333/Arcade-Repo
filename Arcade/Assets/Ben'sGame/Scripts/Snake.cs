14a98af435a37c9cc0cde168e0bc344e27ed9be8
benholcomb23 <ben.holcomb5@gmail.com>
2021-05-07 02:04:44 -0400

    hehe. made it move
    Added arrow key controls to move dot around

--- Arcade/Assets/Ben'sGame/Scripts/Snake.cs
+++ Arcade/Assets/Ben'sGame/Scripts/Snake.cs
@@ -7,19 +7,26 @@ public class Snake : MonoBehaviour
 {
     // Current Movement Direction
     // (by default it moves to the right)
-    Vector2 dir = Vector2.right;
+    Vector2 dir = new Vector2(-0.5f, 0);
 
     // Start is called before the first frame update
     void Start()
     {
         // Move the snake every 300ms
-        InvokeRepeating("Move", 0.4f, 0.4f);
+        InvokeRepeating("Move", 0.2f, 0.2f);
     }
 
     // Update is called once per frame
     void Update()
     {
-
+        if(Input.GetKey(KeyCode.RightArrow))
+            dir = new Vector2(0.5f, 0);
+        else if (Input.GetKey(KeyCode.LeftArrow))
+            dir = new Vector2(-0.5f, 0);
+        else if (Input.GetKey(KeyCode.DownArrow))
+            dir = new Vector2(0, -0.5f);
+        else if (Input.GetKey(KeyCode.UpArrow))
+            dir = new Vector2(0, 0.5f);
     }
     void Move()
     {
