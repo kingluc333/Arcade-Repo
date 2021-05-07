3116cbca69f7cd8b91647d4b52ae3090aeefe672
benholcomb23 <ben.holcomb5@gmail.com>
2021-05-07 03:18:23 -0400

    working with Spawn food
    middle of trying to get food to spawn. still need it to spawn on odd squares

---
+++ Arcade/Assets/Ben'sGame/Scripts/Spawnfood.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Spawnfood : MonoBehaviour
+{
+    // Food Prefab
+    public GameObject foodPrefab;
+
+    // Borders
+    public Transform borderTop;
+    public Transform borderBottom;
+    public Transform borderLeft;
+    public Transform borderRight;
+
+    void Spawn()
+    {
+        // x position between Left and right border
+        int x = (int)Random.Range(borderLeft.position.x *2,
+                                  borderRight.position.x * 2);
+
+        // y position between top and bottom border
+        int y = (int)Random.Range(borderBottom.position.y * 2,
+                                  borderTop.position.y * 2);
+
+        // Instantiate the food at (x, y)
+        Instantiate(foodPrefab,
+                    new Vector2(x / 2, y / 2),
+                    Quaternion.identity);                          
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        InvokeRepeating("Spawn", 3, 4);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
