9de87375af1dd6a52e23f9882a1ba671836017aa
Uka36 <sethnigus6@yahoo.com>
2021-05-09 22:49:11 -0500

    Partway through red block movement

--- Arcade/Assets/Seth's Game/Scripts/Block.cs
+++ Arcade/Assets/Seth's Game/Scripts/Block.cs
@@ -4,42 +4,147 @@ using UnityEngine;
 
 public class Block : MonoBehaviour
 {
+    public GameObject clone;
+
     Vector3 roundedMovement;
+    Vector2[] rot_data;
 
-    // Start is called before the first frame update
-    void Start()
+    //1 - Red, 2 - Green, 3 - Blue, 4 - Orange, 5 - Purple, 6 - Yellow, 7 - Cyan
+    int block_id;
+    //1 - 4
+    int rotation;
+
+    void Start ()
     {
-        
+        //Non-mono, so no start function; have to initialize here
+        Rotation_Data.PopulateDictionary();
+
+        SetBlockID();
+
+        rotation = 1;
     }
 
-    // Update is called once per frame
-    void Update()
+    void Update ()
     {
-        if (Input.GetKeyDown(KeyCode.UpArrow)) {
-            transform.Translate(0f, 0.4f, 0f);
-            RoundMovement();
-        }
-        if (Input.GetKeyDown(KeyCode.RightArrow)) {
-            transform.Translate(0.4f, 0f, 0f);
-            RoundMovement();
-        }
-        if (Input.GetKeyDown(KeyCode.DownArrow)) {
-            transform.Translate(0f, -0.4f, 0f);
-            RoundMovement();
-        }
-        if (Input.GetKeyDown(KeyCode.LeftArrow)) {
-            transform.Translate(-0.4f, 0f, 0f);
-            RoundMovement();
+        GetInput(); //Listens for user input
+        //Fall(); //Initiates checks for falling down
+    }
+
+    //Assigns block ID in "Start" based on name
+    void SetBlockID ()
+    {
+        string name = gameObject.name;
+
+        switch (name)
+        {
+            case "Red": block_id = 1; break;
+            case "Green": block_id = 2; break;
+            case "Blue": block_id = 3; break;
+            case "Orange": block_id = 4; break;
+            case "Purple": block_id = 5; break;
+            case "Yellow": block_id = 6; break;
+            case "Cyan": block_id = 7; break;
         }
     }
 
-    void RoundMovement ()
+    void GetInput ()
     {
-        roundedMovement.x = Mathf.Round(transform.position.x * 1000);
-        roundedMovement.y = Mathf.Round(transform.position.y * 1000);
-        roundedMovement.x /= 1000;
-        roundedMovement.y /= 1000;
+        //Rotate
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            //Check if valid
 
-        transform.position = roundedMovement;
+            //Change rotation index
+            if (rotation == 4) { rotation = 1; } else rotation++;
+
+            //Actual rotation
+            RotateBlock(gameObject);
+        }
+        //Move right
+        if (Input.GetKeyDown(KeyCode.RightArrow))
+        {
+            //Check if valid
+            if (CheckValidMovement(1, rotation)) { transform.Translate(Vector3.right); }
+        }
+        //Move left
+        if (Input.GetKeyDown(KeyCode.LeftArrow))
+        {
+            //Check if valid
+            transform.Translate(Vector3.left);
+        }
+        if (Input.GetKeyDown(KeyCode.Space))
+        {
+            //Jump to bottom
+        }
+
+        //Ability to store pieces as well
     }
+
+    void RotateBlock (GameObject g)
+    {
+        //Give Rotation_Data the rotation and block_id, get a vector array in return
+        rot_data = Rotation_Data.GiveRotationData(block_id, rotation);
+            
+        //Rotate the block
+        for (int i = 0; i < 4; i++) 
+        {
+            g.transform.GetChild(i).transform.localPosition = rot_data[i];
+        }
+    }
+
+    bool CheckValidMovement (int move_type, int rotation)
+    {
+        //Move right
+        if (move_type == 1)
+        {
+            Vector3 v = new Vector3 (transform.position.x + 1, transform.position.y, 0);
+            GameObject c =  Instantiate(clone, v, Quaternion.identity);
+
+            RotateBlock(c);
+
+            bool canMove = false;
+
+            canMove = DoesCloneCollide(c);
+
+            Destroy(c);
+            return !canMove;
+        }
+        //Move left
+        else if (move_type == 2)
+        {
+
+        }
+        //Move down
+        else if (move_type == 3)
+        {
+
+        }
+        //Rotate
+        else if (move_type == 4)
+        {
+
+        }
+
+        return false;
+    }
+
+    bool DoesCloneCollide (GameObject c)
+    {
+        bool b = false;
+        foreach(Transform child in c.transform)
+        {
+            if (child.transform.position.x > 10.5) { b = true; }
+        }
+        return b;
+    }
+
+    // void RoundMovement ()
+    // {
+    //     roundedMovement.x = Mathf.Round(transform.position.x * 1000);
+    //     roundedMovement.y = Mathf.Round(transform.position.y * 1000);
+    //     roundedMovement.x /= 1000;
+    //     roundedMovement.y /= 1000;
+
+    //     transform.position = roundedMovement;
+    // }
 }
---
+++ Arcade/Assets/Seth's Game/Scripts/Rotation_Data.cs
@@ -0,0 +1,71 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Rotation_Data
+{
+    static Vector2[] main_vector_array = new Vector2[4];
+
+    static Dictionary<Vector2, Vector2[]> rot_arrays = new Dictionary<Vector2, Vector2[]>();
+
+    #region Red
+    //Rotation 1
+    static Vector2 r1_1 = new Vector2(0.2f, 0.2f);
+    static Vector2 r1_2 = new Vector2(0.2f, 0.6f);
+    static Vector2 r1_3 = new Vector2(0.6f, 0.2f);
+    static Vector2 r1_4 = new Vector2(-0.2f, 0.6f);
+    static Vector2[] r1 = { r1_1, r1_2, r1_3, r1_4 };
+    //Rotation 2
+    static Vector2 r2_1 = new Vector2(0.2f, 0.2f);
+    static Vector2 r2_2 = new Vector2(0.6f, 0.2f);
+    static Vector2 r2_3 = new Vector2(0.2f, -0.2f);
+    static Vector2 r2_4 = new Vector2(0.6f, 0.6f);
+    static Vector2[] r2 = { r2_1, r2_2, r2_3, r2_4 };
+    //Rotation 3
+    static Vector2 r3_1 = new Vector2(0.2f, 0.2f);
+    static Vector2 r3_2 = new Vector2(0.2f, -0.2f);
+    static Vector2 r3_3 = new Vector2(-0.2f, 0.2f);
+    static Vector2 r3_4 = new Vector2(0.6f, -0.2f);
+    static Vector2[] r3 = { r3_1, r3_2, r3_3, r3_4 };
+    //Rotation 4
+    static Vector2 r4_1 = new Vector2(0.2f, 0.2f);
+    static Vector2 r4_2 = new Vector2(-0.2f, 0.2f);
+    static Vector2 r4_3 = new Vector2(0.2f, 0.6f);
+    static Vector2 r4_4 = new Vector2(-0.2f, -0.2f);
+    static Vector2[] r4 = { r4_1, r4_2, r4_3, r4_4 };
+    #endregion
+    #region Green
+    #endregion
+    #region Blue
+    #endregion
+    #region Orange
+    #endregion
+    #region Purple
+    #endregion
+    #region Yellow
+    #endregion
+    #region Cyan
+    #endregion
+
+    public static Vector2[] GiveRotationData (int block_id, int rotation)
+    {
+        main_vector_array = rot_arrays[new Vector2(block_id, rotation)];
+        return main_vector_array;
+    }
+
+    //Adding all data to dictionary
+    public static void PopulateDictionary ()
+    {
+        //Red
+        rot_arrays.Add(new Vector2(1, 1), r1);
+        rot_arrays.Add(new Vector2(1, 2), r2);
+        rot_arrays.Add(new Vector2(1, 3), r3);
+        rot_arrays.Add(new Vector2(1, 4), r4);
+        //Green
+        //Blue
+        //Orange
+        //Purple
+        //Yellow
+        //Cyan
+    }
+}
